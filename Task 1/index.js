891ea72ca757d879e16e5cb6977c5ef67b5b2697
dammit24 <63319858+dammit24@users.noreply.github.com>
2020-04-18 21:13:48 +0530

    Add files via upload

--- Task 1/index.js
+++ Task 1/index.js
@@ -1,15 +1,26 @@
 let lastClicked =0 ,count =0,rows1,max;
 let elapsedTime;
-let highscore;
+let modes = ['easy','medium','hard'];
 let interval, startTime;
-console.log(localStorage.getItem('highscore'));
-if(localStorage.getItem('highscore') == null){
-    localStorage.setItem('highscore','999');
+
+for(let e of modes)
+if(localStorage.getItem(e) == null){
+    localStorage.setItem(e,'0.000');
 }
+
 function checkHighScore(elapsedTime){
-if(elapsedTime/1000 < Number(localStorage.getItem('highscore'))){
+if(rows1==3){
+if(elapsedTime/1000 < Number(localStorage.getItem('easy')) || Number(localStorage.getItem('easy')) == 0.000){
     highscore = elapsedTime/1000;
-    localStorage.setItem('highscore',highscore.toFixed(3));
+    localStorage.setItem('easy',highscore.toFixed(3));}
+}else if(rows1 == 4){
+    if(elapsedTime/1000 < Number(localStorage.getItem('medium')) || Number(localStorage.getItem('medium')) == 0.000){
+        highscore = elapsedTime/1000;
+        localStorage.setItem('medium',highscore.toFixed(3));}
+}else if (rows1 == 5){
+    if(elapsedTime/1000 < Number(localStorage.getItem('hard')) || Number(localStorage.getItem('hard')) == 0.000){
+        highscore = elapsedTime/1000;
+        localStorage.setItem('hard',highscore.toFixed(3));}
 }
 }
 
@@ -20,12 +31,26 @@ function startTimer(){
         document.getElementById("timer").innerHTML = (elapsedTime / 1000).toFixed(3);
     }, 100);
 }
+function RGB2HTML(red, green, blue)
+{
+    var decColor =0x1000000+ blue + 0x100 * green + 0x10000 *red ;
+    return '#'+decColor.toString(16).substr(1);
+}
+function colourGradient(){
+    let allCells = document.getElementsByTagName('td');
+    let r =110,b =100;
 
+    for(let i=allCells.length -1; i>=0 ;i--){
+        allCells[i].style.backgroundColor = RGB2HTML(r, 0, b);
+        r+=5;
+        b+=5;
+    }
+}
 
 function createTable(rows){
     let grid =document.createElement('table'),
-    tr,cell, k=0,
-    // max=rows*rows;
+    tr,cell, k=0;
+    document.getElementById('timeDisplay').style.display = 'none';
     arr = randomArray(rows);
    rows1 =rows;
     grid.id = 'myTable';
@@ -35,10 +60,13 @@ function createTable(rows){
         for(let j=0;j<rows;j++){
             cell=tr.appendChild(document.createElement('td'));
             cell.innerHTML= arr[k++];
-            cell.addEventListener('click',checkClick);
+            cell.addEventListener('click', checkClick);
         }
     }
     document.getElementById("tableSpace").appendChild(grid);
+    colourGradient();
+    shuffleTable();
+    
     let td =document.getElementById('myTable').classList.add('fadeIn');
     // td[0].classList.add('fadeIn');
 }
@@ -49,6 +77,9 @@ function playAudio(src) {
 function checkClick(){
    if(lastClicked == 0){
        startTimer();
+       if(rows1 ==5){
+        document.getElementById('myTable').classList.add('expert-fadeIn');
+       }
    }
     if(this.innerHTML ==lastClicked+1){
        playAudio('Ding-sound-effect.mp3');
@@ -67,8 +98,9 @@ function checkClick(){
        document.getElementById('timer').innerHTML  = '';
        document.getElementById('timeDisplay').style.display = 'block';
        document.getElementById('timeDisplay').innerHTML = "Your time: " + ((elapsedTime / 1000).toFixed(3)).toString();
-       document.getElementById('highscore').style.display = 'inline';
-       document.getElementById('highscore').innerHTML = 'HIGHSCORE:' +  localStorage.getItem('highscore');
+       document.getElementById('highscoreEasy').innerHTML = 'Highscore Easy:' +  localStorage.getItem('easy');
+       document.getElementById('highscoreMedium').innerHTML = 'Highscore Intermediate:' +  localStorage.getItem('medium');
+       document.getElementById('highscoreHard').innerHTML = 'Highscore Expert:' +  localStorage.getItem('hard');
        document.getElementById('gameMode').style.display = 'block';
    }
 }
@@ -81,12 +113,12 @@ function randomArray(n){
      arr[i] = i +1;
     }
      
-    for ( i = arr.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
-        let temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
+    // for ( i = arr.length - 1; i > 0; i--) {
+    //     let j = Math.floor(Math.random() * (i + 1));
+    //     let temp = arr[i];
+    //     arr[i] = arr[j];
+    //     arr[j] = temp;
+    // }
    return arr;
 }
 
@@ -96,9 +128,11 @@ function shuffleTable(){
     for(let i=allCells.length -1; i>0 ;i--){
         let j = Math.floor(Math.random() * (i + 1));
         if(allCells[i].style.backgroundColor != 'grey' && allCells[j].style.backgroundColor != 'grey'){
-        let temp = allCells[i].innerHTML;
+        let temp = allCells[i].innerHTML, tempBg=allCells[i].style.backgroundColor;
         allCells[i].innerHTML = allCells[j].innerHTML;
-        allCells[j].innerHTML = temp;}
+        allCells[i].style.backgroundColor = allCells[j].style.backgroundColor;
+        allCells[j].innerHTML = temp;
+        allCells[j].style.backgroundColor = tempBg;}
     }
 }
 
@@ -128,7 +162,7 @@ let btnEasy = document.getElementById('btnEasy'),
         lastClicked = 0;
         count=0;
         createTable(5);
-        document.getElementById('myTable').classList.add('expert-fadeIn');
+        // document.getElementById('myTable').classList.add('expert-fadeIn');
         setInterval(shuffleTable, 4000);
     })
 
